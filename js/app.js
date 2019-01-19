659283258c59f7f801ed01425c992f145d846005
Javapocalypse <muhammad.17ali@gmail.com>
2019-01-19 21:45:32 +0500

    Added time and most visited sites

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 (function () {
 
-
+    var bodyElem = document.getElementById('body');
+    var timeElem = document.getElementById('time');
+   
     const url = "https://api.pexels.com/v1/photos/6718/";
     fetch(url, {
         method: 'GET',
@@ -12,16 +14,60 @@
             response => response.json() // .json(), etc.
             // same as function(response) {return response.text();}
         ).then(
-        data => document.getElementById('body').style.backgroundImage = "url(" + data['src']['original'] + ")"
+        data => bodyElem.style.backgroundImage = "url(" + data['src']['original'] + ")"
     );
 
+    // Current Date
     var today = '';
     var d = new Date();
     today += d.getDate() + '-';
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     today += months[d.getMonth()] + '-';
     today += d.getFullYear();
-    console.log(today);
 
+    // Current Time
+    timeElem.innerHTML = getTime();
+
+    // Method to calculate and format time
+    // Returns a string containing time
+    function getTime(){
+        let hr = d.getHours();
+        let min = d.getMinutes();
+        if (min < 10) {
+            min = "0" + min;
+        }
+        let ampm = "am";
+        if( hr > 12 ) {
+            hr -= 12;
+            ampm = "pm";
+        }
+        return hr + ':' + min + ' ' + ampm;
+    }
+
+    function MostVisitedWebsites(mostVisitedURLs) {
+        var popupDiv = document.getElementById('favorites');
+        var ol = popupDiv.appendChild(document.createElement('ol'));
+
+        for (var i = 0; i < mostVisitedURLs.length; i++) {
+            var li = ol.appendChild(document.createElement('li'));
+            var a = li.appendChild(document.createElement('a'));
+            a.href = mostVisitedURLs[i].url;
+            a.appendChild(document.createTextNode(mostVisitedURLs[i].title));
+            a.addEventListener('click', onAnchorClick);
+        }
+    }
+    // Open the link in a new tab of the current window.
+    function onAnchorClick(event) {
+        chrome.tabs.create({ url: event.srcElement.href });
+        return false;
+    }
+
+
+    chrome.topSites.get(MostVisitedWebsites);
+
+
+    //Test Javascript Start
+
+    //Test JavaScript End
 
 })();
